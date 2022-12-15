70ca0f623ddb583cde90cc139fe3d369fde6a1fd
Jean-Paul Balabanian <jepebe@users.noreply.github.com>
2022-12-15 21:05:45 +0100

    AoC 2022 Day15 change to merge intervals

--- aoc2022/day15/day15.py
+++ aoc2022/day15/day15.py
@@ -3,6 +3,17 @@ import aoc
 tester = aoc.Tester("Beacon Exclusion Zone")
 
 
+def merge_intervals(intervals: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    intervals.sort()
+    stack = [intervals[0]]
+    for interval in intervals[1:]:
+        if stack[-1][0] <= interval[0] <= stack[-1][1]:
+            stack[-1] = stack[-1][0], max(stack[-1][1], interval[1])
+        else:
+            stack.append(interval)
+    return stack
+
+
 def distance(t1, t2):
     return abs(t1[0] - t2[0]) + abs(t1[1] - t2[1])
 
@@ -28,9 +39,7 @@ def follow_rim(pos: tuple[int, int], radius: int):
 
 
 def count_where_beacon_is_not_at_row(s_and_b: dict[tuple, tuple], row: int):
-    ext = aoc.find_extents(s_and_b)
-    overlap_min = ext[1]  # max x
-    overlap_max = ext[0]  # min x
+    intervals = []
     exclude = set()
     for sensor, beacon in s_and_b.items():
         if beacon[1] == row:
@@ -44,30 +53,36 @@ def count_where_beacon_is_not_at_row(s_and_b: dict[tuple, tuple], row: int):
             row_min_x = sensor[0] - (sb_dist - sr_dist)
             row_max_x = sensor[0] + (sb_dist - sr_dist)
 
-            if row_min_x < overlap_min:
-                overlap_min = row_min_x
+            intervals.append((row_min_x, row_max_x))
 
-            if row_max_x > overlap_max:
-                overlap_max = row_max_x
+    intervals = merge_intervals(intervals)
+    if len(intervals) > 1:
+        raise UserWarning("More than one interval! O_o")
 
-    return (overlap_max - overlap_min + 1) - len(exclude)
+    return (intervals[0][1] - intervals[0][0] + 1) - len(exclude)
 
 
 def find_beacon_position(s_and_b: dict[tuple, tuple], search_space: int):
     sensor_dist = {s: distance(s, b) for s, b in s_and_b.items()}
 
-    for sensor in s_and_b.keys():
-        dist = sensor_dist[sensor]
-        for rim in follow_rim(sensor, dist):
-            if 0 <= rim[0] <= search_space and 0 <= rim[1] <= search_space:
-                candidate = True
-                for sens, dist in sensor_dist.items():
-                    if distance(sens, rim) <= dist:
-                        candidate = False
-                        break
+    for y in range(search_space + 1):
+        intervals = []
+        for sensor, beacon in s_and_b.items():
+            sb_dist = sensor_dist[sensor]
+            sr_dist = abs(sensor[1] - y)
 
-                if candidate:
-                    return rim[0] * 4000000 + rim[1]
+            if sr_dist <= sb_dist:
+                dist_diff = (sb_dist - sr_dist)
+                row_min_x = sensor[0] - dist_diff
+                row_max_x = sensor[0] + dist_diff
+
+                intervals.append((max(0, row_min_x), min(row_max_x, search_space)))
+
+        intervals = merge_intervals(intervals)
+        if len(intervals) > 1:
+            x = intervals[0][1] + 1
+            return x * 4000000 + y
+
     raise UserWarning("No solution found! ¯\\_(ツ)_/¯")
 
 
